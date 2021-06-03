67150831b810a3cd2d4685ecd72fed2caebc7c92
Ashish K Jha <61977970+ashishjhax@users.noreply.github.com>
2021-06-03 06:44:30 +0530

    Add files via upload
    Implementation in Python.

---
+++ karatsuba.py
@@ -0,0 +1,23 @@
+def karatsuba(num1,num2):
+    if(num1<10 or num2<10):
+        return (num1*num2)
+    else:
+        m=max(len(str(num1)),len(str(num2)))
+        m2=int(m/2)
+        a=int(num1/10**m2)
+        b=int(num1%10**m2)
+        c=int(num2/10**m2)
+        d=int(num2%10**m2)
+
+        z0 = karatsuba(b,d)
+        z1 = karatsuba((a+b),(c+d))
+        z2 = karatsuba(a,c)
+
+        return (z2*10 ** (m2*2)) + ((z1 - z2 - z0)*10**m2) + z0
+
+print(karatsuba(1234,4321))
+
+
+
+
+
